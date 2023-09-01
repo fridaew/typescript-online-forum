5fe4c2f1997ae4fc66e183ab06f06c444c99221f
ulrikaha <98079099+ulrikaha@users.noreply.github.com>
2023-09-01 13:32:22 +0200

    deleted not usable files

--- webshop-typescript/src/App.tsx
+++ webshop-typescript/src/App.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
@@ -8,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and hejsan save to reload.
+          Edit <code>src/App.tsx</code> 
         </p>
         <a
           className="App-link"
@@ -16,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+        
         </a>
       </header>
     </div>
