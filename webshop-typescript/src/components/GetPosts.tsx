e3186734a8b323db224272ef9361040b5e246179
magnuswretblad <mwdev81@gmail.com>
2023-09-01 14:21:43 +0200

    get method

---
+++ webshop-typescript/src/components/GetPosts.tsx
@@ -0,0 +1,29 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
+const GetPosts: React.FC = () => {
+  const [posts, setPosts] = useState<Thread[]>([]);
+
+  useEffect(() => {
+    axios.get('https://jsonplaceholder.typicode.com/posts')
+      .then((response) => {
+        setPosts(response.data);
+      })
+      .catch((error) => {
+        console.error('Error fetching posts:', error);
+      });
+  }, []);
+
+  return (
+    <div>
+      <h2>Get Posts</h2>
+      <ul>
+        {posts.map((post) => (
+          <li key={post.id}>{post.title}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default GetPosts;
