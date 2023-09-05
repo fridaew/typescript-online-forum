7c6850823a6e64522b773158c8066236b52ceb31
fogelmark <108455196+fogelmark@users.noreply.github.com>
2023-09-05 10:25:37 +0200

    for magnus

--- webshop-typescript/src/components/ThreadCommentsView.tsx
+++ webshop-typescript/src/components/ThreadCommentsView.tsx
@@ -55,20 +55,15 @@ import { useState, ChangeEvent } from 'react';
             </div>
             <div className="modal-body">
               <form>
-         
                 <div className="mb-3">
-                  <label htmlFor="recipient-name" className="col-form-label">Title:</label>
-                  <input type="text" className="form-control" id="recipient-name" />
-                </div>
-                <div className="mb-3">
-                  <label htmlFor="message-text" className="col-form-label">Description:</label>
+                  <label htmlFor="message-text" className="col-form-label">Comment</label>
                   <textarea className="form-control" id="message-text"></textarea>
                 </div>
               </form>
             </div>
             <div className="modal-footer">
               <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">CANCEL</button>
-              <button type="button" className="btn btn-primary" onClick={handleAddComment}>SEND <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-send" viewBox="0 0 16 16">
+              <button type="button" className="btn btn-primary" onClick={handleAddComment}>COMMENT <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-send" viewBox="0 0 16 16">
                 <path d="M15.854.146a.5.5 0 0 1 .11.54l-5.819 14.547a.75.75 0 0 1-1.329.124l-3.178-4.995L.643 7.184a.75.75 0 0 1 .124-1.33L15.314.037a.5.5 0 0 1 .54.11ZM6.636 10.07l2.761 4.338L14.13 2.576 6.636 10.07Zm6.787-8.201L1.591 6.602l4.339 2.76 7.494-7.493Z" />
               </svg></button>
             </div>
